c1ff0bc9a7ac71a79798ac99aa7e347f2b35dd4c
d-p1 <rodriguezdiana.p1@gmail.com>
2023-07-20 17:56:53 -0700

    fix for print

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,16 +1,15 @@
 import os
 import csv
 
-#budget = os.path.join("Resources", "budget_data.csv")
 
 budget = r"C:\Users\Diana\Desktop\python-challenge\PyBank\Resources\budget_data.csv"
 
-#PyBank_analysis = os.path.join("analysis", "PyBank_results")
+
 PyBank_analysis = r"C:\Users\Diana\Desktop\python-challenge\PyBank\analysis\PyBank_results.txt"
 
 totalMonths = 0
 monthChanges = []
-changeValues = []  # Initialize as an empty list
+changeValues = []  
 greatestIncrease = ["", 0]
 greatestDecrease = ["", 999]
 totalProfit = 0
@@ -30,7 +29,7 @@ with open(budget) as csvdata:
 
         totalChange = int(row[1]) - previousTotal
         previousTotal = int(row[1])
-        changeValues.append(totalChange)  # Append to the list
+        changeValues.append(totalChange)  
         monthChanges.append(row[0])
 
         if totalChange > greatestIncrease[1]:
@@ -45,7 +44,7 @@ monthlyNet_avg = sum(changeValues) / len(monthChanges)
 
 print(monthlyNet_avg)
 
-# Generate Output Summary
+
 output = (
     f" Financial Analysis:\n"
     f"------------------------\n"
@@ -55,10 +54,9 @@ output = (
     f"Greatest Increase in Profits: {greatestIncrease[0]} (${greatestIncrease[1]})\n"
     f"Greatest Decrease in Profits: {greatestDecrease[0]} (${greatestDecrease[1]})\n")
 
-# Print the output (to terminal)
+
 print(output)
 
-# Export the results to text file
 with open(PyBank_analysis, "w") as txt_file:
     txt_file.write(output)
 
